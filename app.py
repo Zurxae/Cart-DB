7b7df00c40a75956d3b0a175e1e15cd35cea45c3
Steven Zhivov <stevenzhivov1@gmail.com>
2023-03-21 19:45:13 -0400

    code that is able to connect to MySQL

--- app.py
+++ app.py
@@ -1,7 +1,37 @@
-from flask import Flask
+#from flask import Flask
+import mysql.connector
 
-app = Flask(__name__)
+#app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return '<h1>Hello, World!</h1>'
+# @app.route('/')
+# def hello():
+#     return '<h1>Hello, World!</h1>'
+
+
+config = {
+    "host": "localhost",
+    "port": 3306,
+    "database": "cart",
+    "user": "root",
+    "password": "root",
+    "charset": "utf8",
+    "use_unicode": True,
+    "get_warnings": True,
+}
+
+db = mysql.connector.Connect(**config)
+cursor = db.cursor()
+
+refresh_db = "DROP TABLE IF EXISTS users"
+cursor.execute(refresh_db)
+
+create = """
+CREATE TABLE users (
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    fname VARCHAR(50) DEFAULT '' NOT NULL,
+    lname VARCHAR(50) DEFAULT '' NOT NULL,
+    PRIMARY KEY(email)
+) ENGINE=InnoDB"""
+
+cursor.execute(create)
