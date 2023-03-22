61a5e053e594baff56b4722f1053f8b306ee855a
Steven Zhivov <stevenzhivov1@gmail.com>
2023-03-21 21:01:15 -0400

    added files needed for UI

--- app.py
+++ app.py
@@ -1,37 +1,54 @@
-#from flask import Flask
-import mysql.connector
+from flask import Flask, render_template
+#import mysql.connector
 
-#app = Flask(__name__)
+app = Flask(__name__)
 
-# @app.route('/')
-# def hello():
-#     return '<h1>Hello, World!</h1>'
+@app.route('/')
+@app.route('/login/')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup/')
+def login():
+    return render_template('sign-up.html')
+
+@app.route('/editinfo/')
+def login():
+    return render_template('edit-info.html')
+
+@app.route('/menu/')
+def login():
+    return render_template('menu.html')
+
+@app.route('/checkout/')
+def login():
+    return render_template('checkout.html')
 
 
-config = {
-    "host": "localhost",
-    "port": 3306,
-    "database": "cart",
-    "user": "root",
-    "password": "root",
-    "charset": "utf8",
-    "use_unicode": True,
-    "get_warnings": True,
-}
+# config = {
+#     "host": "localhost",
+#     "port": 3306,
+#     "database": "cart",
+#     "user": "root",
+#     "password": "root",
+#     "charset": "utf8",
+#     "use_unicode": True,
+#     "get_warnings": True,
+# }
 
-db = mysql.connector.Connect(**config)
-cursor = db.cursor()
+# db = mysql.connector.Connect(**config)
+# cursor = db.cursor()
 
-refresh_db = "DROP TABLE IF EXISTS users"
-cursor.execute(refresh_db)
+# refresh_db = "DROP TABLE IF EXISTS users"
+# cursor.execute(refresh_db)
 
-create = """
-CREATE TABLE users (
-    email VARCHAR(50) NOT NULL,
-    password VARCHAR(50) NOT NULL,
-    fname VARCHAR(50) DEFAULT '' NOT NULL,
-    lname VARCHAR(50) DEFAULT '' NOT NULL,
-    PRIMARY KEY(email)
-) ENGINE=InnoDB"""
+# create = """
+# CREATE TABLE users (
+#     email VARCHAR(50) NOT NULL,
+#     password VARCHAR(50) NOT NULL,
+#     fname VARCHAR(50) DEFAULT '' NOT NULL,
+#     lname VARCHAR(50) DEFAULT '' NOT NULL,
+#     PRIMARY KEY(email)
+# ) ENGINE=InnoDB"""
 
-cursor.execute(create)
+# cursor.execute(create)
